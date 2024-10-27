73bcf10406bda18d5f6ed6b6a5a77a72037971d8
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-27 13:03:13 +0530

    bug fix

--- src/models/task.models.js
+++ src/models/task.models.js
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 
-
 const TaskSchema = new mongoose.Schema({
   title: String,
   completed: Boolean,
 });
 
-
-
-export const  Task = mongoose.model('Task', TaskSchema)
+export const Task = mongoose.model("Task", TaskSchema);
