fbe34698be00aa07bf003bd601cff366a899cf5b
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-27 16:49:58 +0530

    env fix

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
 import cors from "cors";
 import TaskRouter from "./src/routes/task.routes.js";
-import  connectDB  from "./src/db/db.connection.js";
+import connectDB from "./src/db/db.connection.js";
 import dotenv from "dotenv";
 
+app.use(
+  cors({
+    origin: process.env.CORS,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
+app.use(cors(corsOptions));
 
 const app = express();
 dotenv.config({ path: "./.env" });
@@ -13,8 +21,6 @@ const port = process.env.PORT || 4000;
 connectDB()
   .then(() => console.log("MongoDB connected "))
   .catch((error) => console.log("MongoDB connection error ", error));
-
-app.use(cors({ origin: process.env.CORS || 'https://react-todo-entri.vercel.app',credentials: true }));
 
 app.use(express.json());
 app.use("/task", TaskRouter);
