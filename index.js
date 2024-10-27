b12fe386abd7ebe7c869a988c19cd88c0ec436d4
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-27 16:54:48 +0530

    fix env

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import TaskRouter from "./src/routes/task.routes.js";
 import connectDB from "./src/db/db.connection.js";
 import dotenv from "dotenv";
 
+dotenv.config({ path: "./.env" });
 app.use(
   cors({
     origin: process.env.CORS,
@@ -15,7 +16,6 @@ app.use(
 app.use(cors(corsOptions));
 
 const app = express();
-dotenv.config({ path: "./.env" });
 const port = process.env.PORT || 4000;
 
 connectDB()
