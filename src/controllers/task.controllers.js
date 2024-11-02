cefa2e368ab20a535ca3b8982f5a1d7b16ad6d06
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-02 06:28:50 +0530

    add setTaskComplete

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -1,21 +1,33 @@
 import { Task } from "../models/task.models";
 
-const create = async (req, res) => {
+const createTask = async (req, res) => {
   const task = await Task.create(req.body.data);
 
   res.send(task);
 };
 
-const read = async (req, res) => {
+const readTask = async (req, res) => {
   const tasks = await Task.find({});
 
   res.send(tasks);
 };
 
-const update = async (req, res) => {
+const updateTask = async (req, res) => {
   const id = req.params.id;
   const update = await Task.findByIdAndUpdate(id, req.body.updateData);
   res.send(update);
 };
 
-export { create, read, update };
+const deleteTask = async (req, res) => {
+  const id = req.params.id;
+  const deleted = await Task.findByIdAndDelete(id);
+  res.send(deleted);
+};
+
+const setTaskComplete = async (req, res) => {
+  const id = req.params.id;
+  const update = await Task.findByIdAndUpdate(id, req.body.complete);
+  res.send(update);
+};
+
+export { createTask, readTask, updateTask, deleteTask, setTaskComplete };
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { Task } from "../models/task.models.js";
 import {
   createTask,
   readTask,
   updateTask,
   deleteTask,
+  setTaskComplete,
 } from "../controllers/task.controllers.js";
 
 const router = Router();
@@ -13,11 +13,6 @@ router.get("/", readTask);
 router.post("/", createTask);
 router.put("/:id", updateTask);
 router.delete("/:id", deleteTask);
-
-router.patch("/:id", async (req, res) => {
-  const id = req.params.id;
-  const update = await Task.findByIdAndUpdate(id, req.body.complete);
-  res.send(update);
-});
+router.patch("/:id", setTaskComplete);
 
 export default router;
