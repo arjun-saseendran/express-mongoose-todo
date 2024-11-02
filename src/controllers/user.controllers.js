24c066f7337d806e002226348a5644e804681278
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-02 07:48:19 +0530

    add createUser

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,3 +1,8 @@
 import { User } from "../models/user.models";
 
-const createUser = async (req, res) => {};
+const createUser = async (req, res) => {
+  const user = await User.create(req.body.data);
+  res.send(user);
+};
+
+export { createUser };
