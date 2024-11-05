b1c107bb4cd325da8abc7b71df021dfb509b6eac
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-05 18:52:33 +0530

    add constants

---
+++ constants.js
@@ -0,0 +1,3 @@
+const DB_NAME = "todo";
+
+export { DB_NAME };
--- src/db/db.connection.js
+++ src/db/db.connection.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import { DB_NAME } from "../../constants";
 
-dotenv.config({path:'./.env'})
+dotenv.config({ path: "./.env" });
 
 async function connectDB() {
-  await mongoose.connect(process.env.MONGODB_URL);
+  await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
 }
 
-
-export default connectDB
+export default connectDB;
